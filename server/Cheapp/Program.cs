1276e1b0394c5089d5255df59420982fb0ec1407
Jarczk <36330112+Jarczk@users.noreply.github.com>
2025-03-29 19:24:39 +0100

    add server project

---
+++ server/Cheapp/Program.cs
@@ -0,0 +1,42 @@
+using AspNetCore.Identity.Mongo;
+using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddIdentityMongoDbProvider<MongoUser>(identity =>
+{
+    identity.Password.RequiredLength = 8;
+    identity.Password.RequireDigit = true;
+    // other options
+},
+   mongo =>
+   {
+       mongo.ConnectionString = "mongodb://127.0.0.1:27017/CheappDb";
+       // other options
+   });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
